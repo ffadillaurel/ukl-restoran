ad14e606b74c9a80acfb6716c75dbe726cba3f7c
ffadillaurel <fadilla.aurelia38@smk.belajar.id>
2026-06-02 02:31:17 +0700

    fix tableNumber type in reservation dto

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,7 +1,25 @@
+import { IsString, IsNumber, IsDateString, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsString()
   customerName: string;
+
+  @IsString()
   phoneNumber: string;
+
+  @IsDateString()
   reservationDate: string;
+
+  @Type(() => Number)
+  @IsNumber()
   totalGuest: number;
+
+  @Type(() => Number)
+  @IsNumber()
   tableNumber: number;
+
+  @IsString()
+  @IsOptional()
+  status?: string;
 }
